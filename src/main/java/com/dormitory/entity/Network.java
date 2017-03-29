01342ffecbf0ff6e9c3cc127c2c9192477613b7d
shuangge-jb <562559243@qq.com>
2017-03-29 10:12:33 +0800

    修改了字段名

---
+++ src/main/java/com/dormitory/entity/Network.java
@@ -0,0 +1,46 @@
+package com.dormitory.entity;
+
+public class Network {
+    private Long studentId;
+
+    private String tariffPackage;
+
+    private Integer money;
+
+    public Long getSutdentId() {
+        return studentId;
+    }
+
+    public void setSutdentId(Long sutdentId) {
+        this.studentId = sutdentId;
+    }
+
+    public String getTariffPackage() {
+        return tariffPackage;
+    }
+
+    public void setTariffPackage(String tariffPackage) {
+        this.tariffPackage = tariffPackage == null ? null : tariffPackage.trim();
+    }
+
+    public Integer getMoney() {
+        return money;
+    }
+
+    public void setMoney(Integer money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", sutdentId=").append(studentId);
+        sb.append(", tariffPackage=").append(tariffPackage);
+        sb.append(", money=").append(money);
+        sb.append("]");
+        return sb.toString();
+    }
+}
