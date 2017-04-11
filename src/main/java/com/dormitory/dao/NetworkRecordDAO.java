359af497937d793dddf569274ad0dbc17942dc71
shuangge-jb <562559243@qq.com>
2017-04-11 16:36:39 +0800

    使用@ResultMap注解代替@Results

--- src/main/java/com/dormitory/dao/NetworkRecordDAO.java
+++ src/main/java/com/dormitory/dao/NetworkRecordDAO.java
@@ -4,23 +4,18 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.type.JdbcType;
+
 import com.dormitory.entity.NetworkRecord;
 
 public interface NetworkRecordDAO {
    
 	@Select("select * from network_record r "
 			+ " where  r.student_id=#{0}")
-	@Results(value = {
-			@Result(id = true, property = "networkRecordId", column = "network_record_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "studentId", column = "student_id", javaType = Long.class, jdbcType = JdbcType.BIGINT),
-			@Result(property = "tariffPackage", column = "tariff_package", javaType = String.class, jdbcType = JdbcType.VARCHAR),
-			@Result(property = "money", column = "money", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "createTime", column = "create_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "payTime", column = "pay_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "state", column = "state", javaType = String.class, jdbcType = JdbcType.VARCHAR) })
+	@ResultMap("com.dormitory.mapper.NetworkRecordMapper.networkRecord")
     List<NetworkRecord> listNetworkRecord(Long studentId);
 
     
