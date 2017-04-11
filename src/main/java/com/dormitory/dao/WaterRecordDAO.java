fe33ef9d7601b7858ebf445c561b86e40dceb5f4
shuangge-jb <562559243@qq.com>
2017-04-11 16:26:10 +0800

    将results注解改为resultmap注解

--- src/main/java/com/dormitory/dao/WaterRecordDAO.java
+++ src/main/java/com/dormitory/dao/WaterRecordDAO.java
@@ -4,22 +4,18 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.type.JdbcType;
+
 import com.dormitory.entity.WaterRecord;
 
 public interface WaterRecordDAO {
    
 	@Select("select * from water_record r"
 			+ " where  r.dormitory_id=#{0}")
-	@Results(value = {
-			@Result(id = true, property = "waterRecordId", column = "water_record_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "dormitoryId", column = "dormitory_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "money", column = "money", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "createTime", column = "create_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "payTime", column = "pay_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "state", column = "state", javaType = String.class, jdbcType = JdbcType.VARCHAR) })
+	@ResultMap("com.dormitory.mapper.WaterRecordMapper.waterRecord")
     List<WaterRecord> listWaterRecord(Integer dormitoryId);
 
     
