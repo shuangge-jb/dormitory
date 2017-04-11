abb7ee615b81fe429a6e1791b4d771e565b6e96a
shuangge-jb <562559243@qq.com>
2017-04-11 16:54:50 +0800

    将@Results改成@ResultMap

--- src/main/java/com/dormitory/dao/ElectricityRecordDAO.java
+++ src/main/java/com/dormitory/dao/ElectricityRecordDAO.java
@@ -5,22 +5,17 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.type.JdbcType;
+
 import com.dormitory.entity.ElectricityRecord;
 
 public interface ElectricityRecordDAO {
 	@Select("select * from electricity_record r "
 			+ " where r.dormitory_id=#{0}")
-	@Results(value = {
-			@Result(id = true, property = "electricityRecordId", column = "electricity_record_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "dormitoryId", column = "dormitory_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "money", column = "money", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "createTime", column = "create_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "payTime", column = "pay_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "buyElectricity", column = "buy_electricity", javaType = Double.class, jdbcType = JdbcType.DECIMAL),
-			@Result(property = "state", column = "state", javaType = String.class, jdbcType = JdbcType.VARCHAR) })
+	@ResultMap("com.dormitory.mapper.ElectricityRecordMapper.electricityRecord")
 	public List<ElectricityRecord> listElectricityRecord(Integer dormitoryId);
 
 }
