94792bb11d0b2e1ad15e475f730998a8316b8cfa
shuangge-jb <562559243@qq.com>
2017-04-11 16:50:09 +0800

    删除废弃的注解

--- src/main/java/com/dormitory/dao/WaterDAO.java
+++ src/main/java/com/dormitory/dao/WaterDAO.java
@@ -12,14 +12,6 @@ import com.dormitory.entity.Water;
 
 public interface WaterDAO {
 	@Select("select * from water w  where w.dormitory_id=#{0} ")
-	/*@Results(value = {
-			@Result(id = true, property = "dormitoryId", column = "dormitory_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
-			@Result(property = "prevReadout", column = "prev_readout", javaType = Long.class, jdbcType = JdbcType.BIGINT),
-			@Result(property = "prevTime", column = "prev_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "thisReadout", column = "this_readout", javaType = Long.class, jdbcType = JdbcType.BIGINT),
-			@Result(property = "thisTime", column = "this_time", javaType = Date.class, jdbcType = JdbcType.DATE),
-			@Result(property = "price", column = "price", javaType = Double.class, jdbcType = JdbcType.DATE)
-	})*/
 	@ResultMap("com.dormitory.mapper.WaterMapper.water")
 	Water getWater(Integer dormitoryId);
 
