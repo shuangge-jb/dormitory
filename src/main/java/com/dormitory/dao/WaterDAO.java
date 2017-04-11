bbe346c14b7bfc287b0986878a6f53a65d5eec71
shuangge-jb <562559243@qq.com>
2017-04-11 16:49:34 +0800

    将@Results改成@ResultMap

--- src/main/java/com/dormitory/dao/WaterDAO.java
+++ src/main/java/com/dormitory/dao/WaterDAO.java
@@ -3,21 +3,24 @@ package com.dormitory.dao;
 import java.util.Date;
 
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.type.JdbcType;
+
 import com.dormitory.entity.Water;
 
 public interface WaterDAO {
 	@Select("select * from water w  where w.dormitory_id=#{0} ")
-	@Results(value = {
+	/*@Results(value = {
 			@Result(id = true, property = "dormitoryId", column = "dormitory_id", javaType = Integer.class, jdbcType = JdbcType.INTEGER),
 			@Result(property = "prevReadout", column = "prev_readout", javaType = Long.class, jdbcType = JdbcType.BIGINT),
 			@Result(property = "prevTime", column = "prev_time", javaType = Date.class, jdbcType = JdbcType.DATE),
 			@Result(property = "thisReadout", column = "this_readout", javaType = Long.class, jdbcType = JdbcType.BIGINT),
 			@Result(property = "thisTime", column = "this_time", javaType = Date.class, jdbcType = JdbcType.DATE),
 			@Result(property = "price", column = "price", javaType = Double.class, jdbcType = JdbcType.DATE)
-	})
+	})*/
+	@ResultMap("com.dormitory.mapper.WaterMapper.water")
 	Water getWater(Integer dormitoryId);
 
 }
