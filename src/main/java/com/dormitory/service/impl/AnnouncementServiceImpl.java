a0cae0e4c43cbf4d38ae3b92ec5075bebc19d1ca
shuangge-jb <562559243@qq.com>
2017-04-12 16:57:20 +0800

    实现CRUD方法

--- src/main/java/com/dormitory/service/impl/AnnouncementServiceImpl.java
+++ src/main/java/com/dormitory/service/impl/AnnouncementServiceImpl.java
@@ -3,6 +3,7 @@ package com.dormitory.service.impl;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,61 @@ public class AnnouncementServiceImpl implements AnnouncementService {
 	@Override
 	public List<Announcement> listAnnouncementLimit(Integer n) {
 		return announcementDAO.listAnnouncement(n);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.dormitory.service.AnnouncementService#getLastInsertId()
+	 */
+	@Override
+	public Integer getLastInsertId() {
+		return announcementDAO.getLastInsertId();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.dormitory.service.AnnouncementService#saveAnnouncement(com.dormitory
+	 * .entity.Announcement)
+	 */
+	@Override
+	@Transactional
+	public Announcement saveAnnouncement(Announcement announcement) {
+		Integer oldAnnouncementId = announcement.getAnnouncementId();
+		Announcement temp = announcementDAO.getAnnouncement(oldAnnouncementId);
+		if (temp == null) {
+			announcementDAO.saveAnnouncement(announcement);
+		}
+		return announcement;
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.dormitory.service.AnnouncementService#updateAnnouncement(com.dormitory
+	 * .entity.Announcement)
+	 */
+	@Override
+	@Transactional
+	public Announcement updateAnnouncement(Announcement announcement) {
+		return announcementDAO.updateAnnouncement(announcement);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.dormitory.service.AnnouncementService#removeAnnouncement(java.lang
+	 * .Integer)
+	 */
+	@Override
+	@Transactional
+	public Announcement removeAnnouncement(Integer announcementId) {
+		return announcementDAO.removeAnnouncement(announcementId);
 	}
 
 }
