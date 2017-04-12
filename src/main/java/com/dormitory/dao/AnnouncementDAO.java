663e3dd618ceec13046cead38f276b60e177360b
shuangge-jb <562559243@qq.com>
2017-04-12 18:16:49 +0800

    修改save，update，remove的返回类型为void

--- src/main/java/com/dormitory/dao/AnnouncementDAO.java
+++ src/main/java/com/dormitory/dao/AnnouncementDAO.java
@@ -37,33 +37,30 @@ public interface AnnouncementDAO {
 	 * 插入
 	 * 
 	 * @param announcement
-	 * @return 插入的对象,会修改该对象的id为新增的id
 	 * @author guo.junbao
 	 * @date 2017-4-12
 	 */
-	public Integer saveAnnouncement(
+	public void saveAnnouncement(
 			 Announcement announcement);
 
 	/**
 	 * 修改
 	 * 
 	 * @param announcement
-	 * @return 修改后的对象
 	 * @author guo.junbao
 	 * @date 2017-4-12
 	 */
-	public Announcement updateAnnouncement(
+	public void updateAnnouncement(
 			Announcement announcement);
 
 	/**
 	 * 删除
 	 * 
 	 * @param announcementId
-	 * @return 删除的对象,将其变为无效
 	 * @author guo.junbao
 	 * @date 2017-4-12
 	 */
-	public Announcement removeAnnouncement(
+	public void removeAnnouncement(
 			@Param("announcementId") Integer announcementId);
 
 }
