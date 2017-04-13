9a562176f3b0e4e1a052429425a2e6c2e4c2673b
shuangge-jb <562559243@qq.com>
2017-04-13 12:35:55 +0800

    加入动态SQL构建器

---
+++ src/main/java/com/dormitory/dao/sql/SQLProvider.java
@@ -0,0 +1,15 @@
+package com.dormitory.dao.sql;
+
+import org.apache.ibatis.jdbc.SQL;
+
+public class SQLProvider {
+public String listPostcard(Integer n){
+	String sql=new SQL(){{
+		SELECT("*");
+		FROM("postcard");
+		ORDER_BY("create_time desc");
+	}}.toString();
+	sql+=("limit"+n);
+	return sql;
+}
+}
