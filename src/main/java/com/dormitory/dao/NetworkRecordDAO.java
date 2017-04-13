ab9b95efaae5c5e43ffe716fc2e047aa5ec266e0
shuangge-jb <562559243@qq.com>
2017-04-13 13:17:59 +0800

    删除废弃的import

--- src/main/java/com/dormitory/dao/NetworkRecordDAO.java
+++ src/main/java/com/dormitory/dao/NetworkRecordDAO.java
@@ -1,14 +1,9 @@
 package com.dormitory.dao;
 
-import java.util.Date;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.type.JdbcType;
-
 import com.dormitory.entity.NetworkRecord;
 
 public interface NetworkRecordDAO {
