72ced85b23d303fca986924a6d5ee912d8e3296c
shuangge-jb <562559243@qq.com>
2017-04-13 10:17:04 +0800

    完成CRUD

---
+++ src/main/java/com/dormitory/service/PostcardService.java
@@ -0,0 +1,66 @@
+package com.dormitory.service;
+
+import java.util.List;
+
+import com.dormitory.entity.Postcard;
+
+public interface PostcardService {
+	/**
+	 * 按时间降序找出所有对象
+	 * 
+	 * @return
+	 */
+	public List<Postcard> listPostcard();
+
+	/**
+	 * 按时间降序找出前n个对象
+	 * 
+	 * @param n
+	 * @return
+	 */
+	public List<Postcard> listPostcardLimit(Integer n);
+
+	/**
+	 * 找出该学生的所有postcard对象
+	 * 
+	 * @param studentId
+	 * @return
+	 */
+	public List<Postcard> listPostcardByStudentId(Long studentId);
+
+	/**
+	 * 找出一个对象
+	 * 
+	 * @param postcardId
+	 * @return
+	 */
+	public Postcard getPostcard(Integer postcardId);
+
+	/**
+	 * 找出最近插入的主键
+	 * 
+	 * @return
+	 */
+	public Integer getLastInsertId();
+
+	/**
+	 * 插入
+	 * 
+	 * @param postcard
+	 */
+	public void savePostcard(Postcard postcard);
+
+	/**
+	 * 修改
+	 * 
+	 * @param postcard
+	 */
+	public void updatePostcard(Postcard postcard);
+
+	/**
+	 * 删除
+	 * 
+	 * @param postcard
+	 */
+	public void removePostcard(Postcard postcard);
+}
