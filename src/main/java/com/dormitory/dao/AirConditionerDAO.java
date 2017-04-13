305d5012bec425a215918867a65130f14cf652c3
shuangge-jb <562559243@qq.com>
2017-04-13 13:14:55 +0800

    删除废弃的import

--- src/main/java/com/dormitory/dao/AirConditionerDAO.java
+++ src/main/java/com/dormitory/dao/AirConditionerDAO.java
@@ -1,15 +1,9 @@
 package com.dormitory.dao;
 
-import java.util.List;
-
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-
 import com.dormitory.entity.AirConditioner;
-import com.dormitory.entity.RepairRecord;
 
 public interface AirConditionerDAO {
 	@Select("select * from air_conditioner where dormitory_id=#{dormitoryId}")
