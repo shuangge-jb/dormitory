fa4a37ddd78a7f3c8ceb6a1deec4b197c3660da1
shuangge-jb <562559243@qq.com>
2017-04-20 15:45:31 +0800

    修正了动态sql

--- src/main/java/com/dormitory/dao/sql/SQLProvider.java
+++ src/main/java/com/dormitory/dao/sql/SQLProvider.java
@@ -3,13 +3,17 @@ package com.dormitory.dao.sql;
 import org.apache.ibatis.jdbc.SQL;
 
 public class SQLProvider {
-public String listPostcard(Integer n){
-	String sql=new SQL(){{
-		SELECT("*");
-		FROM("postcard");
-		ORDER_BY(" create_time desc ");
-	}}.toString();
-	sql+=(" limit "+n);
-	return sql;
-}
+	public String listPostcard(Integer n) {
+		String sql = new SQL() {
+			{
+				SELECT("*");
+				FROM("postcard");
+				ORDER_BY(" create_time desc ");
+			}
+		}.toString();
+		if (n != null) {
+			sql += (" limit " + n);
+		}
+		return sql;
+	}
 }
