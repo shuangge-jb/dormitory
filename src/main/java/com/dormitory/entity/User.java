20b765e14cb22567f4e7f6e7fc1da10a013f3367
shuangge-jb <562559243@qq.com>
2017-04-21 23:57:45 +0800

    增加校验注解

--- src/main/java/com/dormitory/entity/User.java
+++ src/main/java/com/dormitory/entity/User.java
@@ -1,12 +1,26 @@
 package com.dormitory.entity;
 
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public abstract class User {
+	@NotEmpty(message = "{user.name.null}")
+	@Length(min = 5, max = 20, message = "{user.name.length.illegal}")
+	@Pattern(regexp = "[a-zA-Z]{5,20}", message = "{user.name.illegal}")
 	protected String name;
-
+	@NotEmpty(message = "{user.phoneNumber.null}")
+	@Length(min = 11, max = 11, message = "{user.phoneNumber.length.illegal}")
+	@Pattern(regexp = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$", message = "{user.phoneNumber.illegal}")
 	protected Long phoneNumber;
-
+	@NotEmpty(message = "{user.email.null}")
+	@Length(min =5, max = 50, message = "{user.email.length.illegal}")
+	@Pattern(regexp = "^[A-Za-z0-9"+"\\u4e00-\\u9fa5]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$ ", message = "{user.email.illegal}")
 	protected String email;
-
+	@NotEmpty(message = "{user.password.null}")
+	@Length(min = 5, max = 20, message = "{user.password.length.illegal}")
+	@Pattern(regexp = "[a-zA-Z]{5,20}", message = "{user.password.illegal}")
 	protected String password;
 
 	public String getName() {
