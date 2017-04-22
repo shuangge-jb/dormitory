56f9746af56b516e5ebc1540ce9d6a48e0f58c91
shuangge-jb <562559243@qq.com>
2017-04-22 19:27:16 +0800

    完成了file类型校验

--- src/main/java/com/dormitory/controller/ArticleController.java
+++ src/main/java/com/dormitory/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.dormitory.controller;
 
 import java.io.File;
+import java.io.IOException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -12,57 +13,84 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.dormitory.validator.FileType;
 import com.dormitory.entity.Article;
 import com.dormitory.service.ArticleService;
-
+import com.dormitory.service.dto.ArticleDTO;
+import com.dormitory.validator.ArticleDTOValidator;
 
 @RequestMapping(value = "/article")
 @SessionAttributes({ "studentId", "administratorId", "dormitoryId" })
 @Controller
 public class ArticleController {
 	private static final String DIR = "/models/";
-	private static final String ERROR_PAGE="error";
+	private static final String ERROR_PAGE = "error";
 	private static final Logger LOGGER = LoggerFactory.getLogger(ArticleController.class);
-	
+
 	@Autowired
 	private ArticleService articleService;
 
+	/*@InitBinder   
+    protected   void  initBinder(WebDataBinder binder){  
+        binder.setValidator(new  ArticleDTOValidator());  
+    } */
+	
 	@RequestMapping(value = "/saveArticle", method = RequestMethod.POST)
-	public String saveArticle(@ModelAttribute(value = "article") @Valid Article article, BindingResult result,
-			@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request,
+	public String saveArticle(@ModelAttribute(value = "article") @Valid ArticleDTO articleDTO, BindingResult articldDTOResult,
+			@RequestParam(value = "file", required = false)  MultipartFile file,HttpServletRequest request,
 			Model model) {
-		if (result.hasErrors()) {
+		if (articldDTOResult.hasErrors()) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("参数异常：result.hasErrors."+articldDTOResult);
+			}
 			return ERROR_PAGE;
 		}
-		String path = request.getSession().getServletContext().getRealPath("models");
+		if(!(file.getName().endsWith(".obj")||file.getName().endsWith(".dae"))){
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("file参数异常：");
+			}
+			return ERROR_PAGE;
+		}
+		String path = request.getSession().getServletContext().getRealPath("/");
+		System.out.println("path:"+path);
 		String contextPath = request.getContextPath();
+		//System.out.println("contextPath:"+contextPath);
 		String fileName = file.getOriginalFilename();
-		System.out.println(path);
+		System.out.println("fileName:"+fileName);
 		File targetFile = new File(path, fileName);
 		if (!targetFile.exists()) {
 			targetFile.mkdirs();
 		}
 		String modelUrl = contextPath + DIR + fileName;
-		article.setPath(modelUrl);
-		System.out.println("saveArticle:" + article);
-		// articleService.saveOrUpdate(article);
+		articleDTO.setPath(modelUrl);
+		System.out.println("saveArticle:" + articleDTO);
+		// articleService.saveOrUpdate((Article)article);
 		model.addAttribute("path", modelUrl);
 		// 保存
+
 		try {
 			file.transferTo(targetFile);
-		} catch (Exception e) {
+		} catch (IllegalStateException e1) {
 			if (LOGGER.isDebugEnabled()) {
-				LOGGER.debug("异常：saveArticle exception:", e);
+				LOGGER.debug("IllegalStateException at saveArticle:", e1);
 			}
-
 			return ERROR_PAGE;
+		} catch (IOException e1) {
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("IOException at saveArticle:", e1);
+			}
+			return ERROR_PAGE;
+
 		}
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("成功上传文件.");
