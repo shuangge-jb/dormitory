e2cceaf9cab8f86a97e6719cb0abba85f0627cb7
shuangge-jb <562559243@qq.com>
2017-04-22 23:00:36 +0800

    增加一个方法用于注册

--- src/main/java/com/dormitory/service/impl/StudentServiceImpl.java
+++ src/main/java/com/dormitory/service/impl/StudentServiceImpl.java
@@ -1,19 +1,26 @@
 package com.dormitory.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.dormitory.dao.DormitoryDAO;
 import com.dormitory.dao.StudentDAO;
+import com.dormitory.entity.Dormitory;
 import com.dormitory.entity.Student;
 import com.dormitory.entity.User;
 import com.dormitory.service.StudentService;
+import com.dormitory.service.dto.RegisterDTO;
 
 @Service
 public class StudentServiceImpl implements StudentService {
 	@Resource
 	private StudentDAO studentDAO;
+	@Resource
+	DormitoryDAO dormitoryDAO;
 
 	@Override
 	public Student get(Long studentId) {
@@ -29,18 +36,36 @@ public class StudentServiceImpl implements StudentService {
 		this.studentDAO = studentDAO;
 	}
 
-	
+	public String saveOrUpdate(RegisterDTO registerDTO) {
+		Student student = (Student) registerDTO;
+		Student temp = studentDAO.get(student.getStudentId());
+		if (temp == null) {
+			Dormitory dormitory = registerDTO.getDormitory();
+			List<Dormitory> list = dormitoryDAO.getByBuildingAndRoom(dormitory);
+			if (list.isEmpty()) {
+				dormitoryDAO.save(dormitory);
+			} else {
+				studentDAO.save(student);
+			}
+		}else{
+			return "已注册";
+		}
+		return null;
+
+	}
+
 	@Transactional
 	@Override
 	public Student saveOrUpdate(Student student) {
-		Student temp=studentDAO.get(student.getStudentId());
-		if(temp==null){
+		Student temp = studentDAO.get(student.getStudentId());
+		if (temp == null) {
 			studentDAO.save(student);
-		}else{
+		} else {
 			studentDAO.update(student);
 		}
 		return null;
 	}
+
 	@Transactional
 	@Override
 	public Student remove(Student student) {
