dddc79a2a98c398da605fbfb503920baa774c136
shuangge-jb <562559243@qq.com>
2017-04-22 19:26:51 +0800

    去掉了自定义注解

--- src/main/java/com/dormitory/service/dto/ArticleDTO.java
+++ src/main/java/com/dormitory/service/dto/ArticleDTO.java
@@ -6,14 +6,26 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.dormitory.validator.FileType;
 import com.dormitory.entity.Article;
 
 public class ArticleDTO extends Article {
-	@NotEmpty(message = "{article.file.null}")
+	//@NotEmpty(message = "{article.file.null}")
+	//@FileType(message="{article.file.type.illegal}")
 	protected File file;
 
 	public ArticleDTO() {
 		// TODO Auto-generated constructor stub
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleDTO [file=" + file + ", articleId=" + articleId + ", dormitoryId=" + dormitoryId + ", studentId="
+				+ studentId + ", name=" + name + ", type=" + type + ", state=" + state + ", path=" + path + "]";
 	}
 
 }
