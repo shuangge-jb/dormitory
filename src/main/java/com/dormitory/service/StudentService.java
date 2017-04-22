44e713f9856ffe454eeee0951c07d21ff22df982
shuangge-jb <562559243@qq.com>
2017-04-22 22:58:13 +0800

    增加一个方法用于注册

--- src/main/java/com/dormitory/service/StudentService.java
+++ src/main/java/com/dormitory/service/StudentService.java
@@ -1,7 +1,8 @@
 package com.dormitory.service;
 
 import com.dormitory.entity.Student;
+import com.dormitory.service.dto.RegisterDTO;
 
 public interface StudentService extends GetService<Student, Long>,SaveOrUpdateService<Student>,RemoveService<Student> {
-
+	public String saveOrUpdate(RegisterDTO registerDTO);
 }
