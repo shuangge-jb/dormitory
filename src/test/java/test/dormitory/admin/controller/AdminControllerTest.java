bafffbd1d3d0f98bc7ea4745715b9ef1f85428d8
shuangge-jb <562559243@qq.com>
2017-04-23 19:26:51 +0800

    测试管理员登录方法，测试通过

---
+++ src/test/java/test/dormitory/admin/controller/AdminControllerTest.java
@@ -0,0 +1,51 @@
+package test.dormitory.admin.controller;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import javax.annotation.Resource;
+import javax.ws.rs.core.MediaType;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.dormitory.admin.controller.AdminController;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "classpath:spring-mvc.xml", "classpath:applicationContext.xml",
+		"classpath:spring-mybatis.xml" })
+public class AdminControllerTest {
+	@Resource
+	private AdminController adminController;
+	@Resource
+	private WebApplicationContext wac;
+	private MockMvc mockMvc;
+
+	@Before
+	public void setup() throws Exception {
+		// this.mockMvc = webAppContextSetup(this.wac).build();
+		mockMvc = MockMvcBuilders.standaloneSetup(adminController).build();
+
+	}
+
+	@Test
+	public void testLogin() {
+		try {
+			mockMvc.perform(get("/admin/login").param("name", "admin").param("password", "admin"))
+					.andExpect(view().name("redirect:/admin")).andDo(print());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
