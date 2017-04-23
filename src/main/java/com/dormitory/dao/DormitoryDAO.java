3e0cf2fab8829aec878bff554cc04eddd13fbef6
shuangge-jb <562559243@qq.com>
2017-04-23 19:20:14 +0800

    增加getByBuildingAndRoom方法

--- src/main/java/com/dormitory/dao/DormitoryDAO.java
+++ src/main/java/com/dormitory/dao/DormitoryDAO.java
@@ -1,7 +1,14 @@
 package com.dormitory.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
 import com.dormitory.entity.Dormitory;
 
 public interface DormitoryDAO {
@@ -10,5 +17,13 @@ public interface DormitoryDAO {
 	@ResultMap("com.dormitory.mapper.DormitoryMapper.dormitory")
 	Dormitory get(Integer dormitoryId);
 
+	@Insert(" insert into dormitory(building,room) values(#{building},#{room}) ")
+	@Options(useGeneratedKeys = true, keyProperty = "dormitoryId")
+	void save(Dormitory dormitory);
+	@Update(" update dormitory set building=#{building},room=#{room} where dormitory_id=#{dormitoryId} ")
+	void update(Dormitory dormitory);
+	@Delete("delete from dormitory where dormitory_id=#{dormitoryId} ")
+	void remove(Dormitory dormitory);
 	
+	List<Dormitory> getByBuildingAndRoom(Dormitory dormitory);
 }
