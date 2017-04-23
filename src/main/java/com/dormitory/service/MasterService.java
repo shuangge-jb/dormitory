729f285da08ebf4aff871af304aa68a7fbb47c73
shuangge-jb <562559243@qq.com>
2017-04-23 19:31:15 +0800

    改名

---
+++ src/main/java/com/dormitory/service/MasterService.java
@@ -0,0 +1,8 @@
+package com.dormitory.service;
+
+import com.dormitory.entity.Master;
+
+public interface MasterService
+		extends GetService<Master, Integer>, SaveOrUpdateService<Master>, RemoveService<Master> {
+
+}
