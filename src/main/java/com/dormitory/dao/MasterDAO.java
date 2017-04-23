3e64ab3be72b6bcefc593488af2b522285a2731f
shuangge-jb <562559243@qq.com>
2017-04-23 19:20:29 +0800

    改名

---
+++ src/main/java/com/dormitory/dao/MasterDAO.java
@@ -0,0 +1,32 @@
+package com.dormitory.dao;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.dormitory.entity.Master;
+
+public interface MasterDAO {
+	@Select("select * from master where master_id=#{masterId}")
+	@ResultMap("com.dormitory.mapper.MasterMapper.master")
+	public Master get(@Param("masterId") Integer id);
+
+	@Select("select LAST_INSERT_ID()")
+	public Integer getLastInsertId();
+
+	@Insert(" insert into master(name,phone_number,email,password) "
+			+ " values(#{name},#{phoneNumber},#{email},#{password})")
+	@Options(useGeneratedKeys = true, keyProperty = "masterId")
+	public void save(Master master);
+
+	@Update("update master set name=#{name},phone_number=#{phoneNumber}, "
+			+ " email=#{email},password=#{password}")
+	public void update(Master master);
+
+	@Delete("delete from master where master=#{masterId}")
+	public void remove(Master master);
+}
