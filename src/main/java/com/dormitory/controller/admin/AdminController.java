a7e41e35f7109814052ea88ebd5ff505f29672d5
shuangge-jb <562559243@qq.com>
2017-04-24 22:46:21 +0800

    用于实现管理员的所有功能

---
+++ src/main/java/com/dormitory/controller/admin/AdminController.java
@@ -0,0 +1,26 @@
+package com.dormitory.controller.admin;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.dormitory.service.MasterService;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	@Resource
+	private MasterService adminService;
+
+	@RequestMapping("/login")
+	public String login(@RequestParam(value = "name") String name, @RequestParam(value = "password") String password) {
+		if (name.equals("admin") && password.equals("admin")) {
+			return "redirect:/admin";
+		} else {
+			return "redirect:/login";
+		}
+
+	}
+}
