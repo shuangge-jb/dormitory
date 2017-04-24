bf87227a54efc38677d8343eee6f449ffc20a3e4
shuangge-jb <562559243@qq.com>
2017-04-24 12:47:22 +0800

    修改了login方法

--- src/main/java/com/dormitory/student/controller/StudentController.java
+++ src/main/java/com/dormitory/student/controller/StudentController.java
@@ -1,13 +1,12 @@
 package com.dormitory.student.controller;
 
+import java.util.Map;
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,13 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.dormitory.entity.Dormitory;
 import com.dormitory.entity.Student;
 import com.dormitory.service.DormitoryService;
+import com.dormitory.service.EmailService;
 import com.dormitory.service.StudentService;
 import com.dormitory.service.dto.RegisterDTO;
-import com.dormitory.validator.ArticleDTOValidator;
-import com.dormitory.validator.RegisterDTOValidator;
 
 @SessionAttributes({ "studentId", "dormitoryId" })
 @Controller
@@ -32,6 +29,8 @@ public class StudentController {
 	private StudentService studentService;
 	@Resource
 	private DormitoryService dormitoryService;
+	@Resource
+	private EmailService emailService;
 
 	public StudentController() {
 
@@ -59,15 +58,19 @@ public class StudentController {
 
 	@RequestMapping("/login")
 	public String login(@RequestParam(value = "studentId") String studentId,
-			@RequestParam(value = "name") String password) {
+			@RequestParam(value = "password") String password) {
 
 		Student temp = studentService.get(Long.valueOf(studentId.trim()));
+		System.out.println("temp:"+temp);
 		if (temp != null) {
-			if (password.trim() == temp.getPassword()) {
+			if (password.trim().equals(temp.getPassword())) {
 				return "redirect:/student";
+			}else{
+				return "redirect:/login";
 			}
+				
 		}
-		System.out.println("login success");
+		System.out.println("login fail");
 		return "redirect:/login";
 	}
 
@@ -90,14 +93,41 @@ public class StudentController {
 	}
 
 	@RequestMapping("/updatePassword")
-	public String updatePassword(@ModelAttribute(value = "student") Student student) {
-		return null;
+	public String updatePassword(@RequestParam(value = "studentId")String studentId) {
+		Student student=studentService.get(Long.valueOf(studentId));
+		if(student==null){
+			return "redirect:/error";
+		}
+		studentService.saveOrUpdate(student);
+		return "redirect:/login";
 
 	}
 
-	@RequestMapping("/findPassword")
-	public String findPassword(@ModelAttribute(value = "studentId") String studentId) {
-		return null;
+	@RequestMapping("/forgetPassword")
+	public Map<String,String> forgetPassword(@RequestParam(value = "request") HttpServletRequest request,
+			@RequestParam(value = "studentId") String studentId) {
+		Long id = Long.valueOf(studentId.trim());
+		String path = request.getContextPath();
+		String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path
+				+ "/";
+		Student student = studentService.get(id);
+		return emailService.sendEmail(student, basePath);
+	}
 
+	/**
+	 * 找回链接已经发到邮箱了。进入邮箱点开链接 以下为链接检验代码，验证通过 跳转到修改密码界面,否则跳转到失败界面
+	 */
+	@RequestMapping(value = "/checkResetLink", method = RequestMethod.GET)
+	public ModelAndView checkResetLink(String sid, String id) {
+		ModelAndView model = new ModelAndView("redirect:/error");
+		Map<String, String> map = emailService.checkResetLink(sid, Long.valueOf(id.trim()));
+		if (map.get("status").equals("success")) {
+			model.setViewName("/updatePassword"); // 返回到修改密码的界面
+			model.addObject("studentId", id);
+			return model;
+		} else {
+			model.addObject("msg", map.get("msg"));
+			return model;
+		}
 	}
 }
