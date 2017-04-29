713e60e1b853ec7e0aacfe105724639b0e1d2216
shuangge-jb <562559243@qq.com>
2017-04-29 16:22:01 +0800

    访问外部系统

---
+++ src/main/java/com/dormitory/service/impl/HTTPServiceImpl.java
@@ -0,0 +1,15 @@
+package com.dormitory.service.impl;
+
+import java.util.Map;
+
+import com.dormitory.service.HTTPService;
+
+public class HTTPServiceImpl implements HTTPService {
+
+	@Override
+	public String getData(String url, Map<String, String> paramater) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
