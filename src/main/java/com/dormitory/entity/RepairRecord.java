6c725d5e06a1b0a403d0c5c89e7a17657f56b903
shuangge-jb <562559243@qq.com>
2017-04-29 16:33:04 +0800

    增加了属性

--- src/main/java/com/dormitory/entity/RepairRecord.java
+++ src/main/java/com/dormitory/entity/RepairRecord.java
@@ -3,69 +3,65 @@ package com.dormitory.entity;
 import java.sql.Timestamp;
 
 public class RepairRecord {
-    protected Integer repairRecordId;
+	protected Integer repairRecordId;
+	protected Integer dormitoryId;
+	protected String deviceName;
+	protected String content;
+	protected Double price;
+	protected Integer state;
+	protected Timestamp createTime;
+	protected Timestamp repairTime;
+	protected Long contactId;
 
-    protected Integer dormitoryId;
+	public Integer getRepairRecordId() {
+		return repairRecordId;
+	}
 
-    protected String content;
+	public void setRepairRecordId(Integer repairRecordId) {
+		this.repairRecordId = repairRecordId;
+	}
 
-    protected Integer state;
+	public Integer getDormitoryId() {
+		return dormitoryId;
+	}
 
-    protected Timestamp createTime;
+	public void setDormitoryId(Integer dormitoryId) {
+		this.dormitoryId = dormitoryId;
+	}
 
-    protected Timestamp repairTime;
+	public String getContent() {
+		return content;
+	}
 
-    protected Long contactId;
-    
-    public Integer getRepairRecordId() {
-        return repairRecordId;
-    }
+	public void setContent(String content) {
+		this.content = content == null ? null : content.trim();
+	}
 
-    public void setRepairRecordId(Integer repairRecordId) {
-        this.repairRecordId = repairRecordId;
-    }
+	public Integer getState() {
+		return state;
+	}
 
-    public Integer getDormitoryId() {
-        return dormitoryId;
-    }
+	public void setState(Integer state) {
+		this.state = state;
+	}
 
-    public void setDormitoryId(Integer dormitoryId) {
-        this.dormitoryId = dormitoryId;
-    }
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
 
-    public void setContent(String content) {
-        this.content = content == null ? null : content.trim();
-    }
+	public Timestamp getRepairTime() {
+		return repairTime;
+	}
 
-    public Integer getState() {
-        return state;
-    }
+	public void setRepairTime(Timestamp repairTime) {
+		this.repairTime = repairTime;
+	}
 
-    public void setState(Integer state) {
-        this.state = state;
-    }
-
-    public Timestamp getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Timestamp createTime) {
-        this.createTime = createTime;
-    }
-
-    public Timestamp getRepairTime() {
-        return repairTime;
-    }
-
-    public void setRepairTime(Timestamp repairTime) {
-        this.repairTime = repairTime;
-    }
-
-    public Long getContactId() {
+	public Long getContactId() {
 		return contactId;
 	}
 
@@ -73,20 +69,27 @@ public class RepairRecord {
 		this.contactId = contactId;
 	}
 
+	public String getDeviceName() {
+		return deviceName;
+	}
+
+	public void setDeviceName(String deviceName) {
+		this.deviceName = deviceName;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
 	@Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", repairRecordId=").append(repairRecordId);
-        sb.append(", dormitoryId=").append(dormitoryId);
-        sb.append(", content=").append(content);
-        sb.append(", state=").append(state);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", repairTime=").append(repairTime);
-        sb.append(", contactId=").append(contactId);
-        sb.append("]");
-        return sb.toString();
-    }
+	public String toString() {
+		return "RepairRecord [repairRecordId=" + repairRecordId + ", dormitoryId=" + dormitoryId + ", deviceName="
+				+ deviceName + ", content=" + content + ", price=" + price + ", state=" + state + ", createTime="
+				+ createTime + ", repairTime=" + repairTime + ", contactId=" + contactId + "]";
+	}
+
 }
