f58beeac6fe4b96386ec77fa9e1702d1f2145719
shuangge-jb <562559243@qq.com>
2017-04-29 16:24:33 +0800

    接口参数的相关服务

---
+++ src/main/java/com/dormitory/service/ParamaterService.java
@@ -0,0 +1,8 @@
+package com.dormitory.service;
+
+import com.dormitory.entity.Paramater;
+
+public interface ParamaterService
+		extends GetService<Paramater, Integer>, SaveOrUpdateService<Paramater>, RemoveService<Paramater> {
+
+}
