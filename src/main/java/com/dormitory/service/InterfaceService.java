d4958732900b0df7e9c5d4bc2f68b3c91260e676
shuangge-jb <562559243@qq.com>
2017-04-29 16:23:48 +0800

    接口服务

---
+++ src/main/java/com/dormitory/service/InterfaceService.java
@@ -0,0 +1,8 @@
+package com.dormitory.service;
+
+import com.dormitory.entity.Interface;
+
+public interface InterfaceService
+		extends GetService<Interface, Integer>, SaveOrUpdateService<Interface>, RemoveService<Interface> {
+
+}
