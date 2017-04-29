cb4b705af7c296693dbb02b154ad5849bfaabfa7
shuangge-jb <562559243@qq.com>
2017-04-29 20:47:33 +0800

    查询返回JSON字符串

--- src/main/java/com/dormitory/controller/AbstractLostFoundController.java
+++ src/main/java/com/dormitory/controller/AbstractLostFoundController.java
@@ -1,5 +1,6 @@
 package com.dormitory.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -8,11 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.dormitory.controller.student.StudentController;
 import com.dormitory.entity.LostFound;
 import com.dormitory.service.LostFoundService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public abstract class AbstractLostFoundController {
@@ -23,17 +27,30 @@ public abstract class AbstractLostFoundController {
 	protected static final String REMOVE_SUCCESS = "删除成功";
 	protected static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
 	@RequestMapping(value = "listLostFound.do")
-	public ModelAndView listLostFound(Integer pageIndex, Integer pageSize) {
-		ModelAndView modelAndView = new ModelAndView("homePage");
+	@ResponseBody
+	public String listLostFound(Integer pageIndex, Integer pageSize) {
 		List<LostFound> list = lostFoundService.list(pageIndex, pageSize);
-		modelAndView.addObject("lostFound", list);
-		return modelAndView;
+		return toJSON(list);
 	}
 	@RequestMapping(value = "listLostFoundByStudentId.do")
-	public ModelAndView listLostFoundByStudentId(Long studentId,Integer pageIndex, Integer pageSize) {
-		ModelAndView modelAndView = new ModelAndView("homePage");
+	@ResponseBody
+	public String listLostFoundByStudentId(Long studentId,Integer pageIndex, Integer pageSize) {
 		List<LostFound> list = lostFoundService.listByStudentId(studentId,pageIndex, pageSize);
-		modelAndView.addObject("lostFound", list);
-		return modelAndView;
+		return toJSON(list);
+	}
+	protected String toJSON(Object obj) {
+		ObjectMapper mapper = new ObjectMapper();
+		SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		mapper.setDateFormat(format);
+		String result = null;
+		try {
+			result = mapper.writeValueAsString(obj);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			if (LOGGER.isDebugEnabled()) {
+				LOGGER.debug("序列化Announcement对象时出错", e);
+			}
+		}
+		return result;
 	}
 }
