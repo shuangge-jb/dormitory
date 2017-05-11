bf445d1c08226c6a728572874f313f79e74eff43
shuangge-jb <562559243@qq.com>
2017-05-11 23:49:26 +0800

    保存更改

---
+++ src/main/java/com/dormitory/interceptor/StudentLoginInterceptor.java
@@ -0,0 +1,67 @@
+package com.dormitory.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+/**
+ * 登录认证的拦截器 
+ * @author guo.junbao
+ * @date 2017-5-11
+ *
+ */
+public class StudentLoginInterceptor implements HandlerInterceptor {
+	private String backUrl=null;
+	/**
+	 * Handler执行完成之后调用这个方法 
+	 */
+	@Override
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		
+	}
+/**
+ * Handler执行之后，ModelAndView返回之前调用这个方法 
+ */
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response,
+			Object handler, ModelAndView mv) throws Exception {
+		if(backUrl!=null&&(backUrl.isEmpty()==false)){
+			StringBuffer loginUrl=request.getRequestURL();
+			response.sendRedirect(loginUrl + "?backurl=" + backUrl); 
+		}
+		
+	}
+/**
+ * Handler执行之前调用这个方法 
+ */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object handler) throws Exception {
+		 //获取请求的URL  
+        String url = request.getRequestURI();  
+        backUrl=url;
+        //URL:login.jsp是公开的;这个demo是除了login.jsp是可以公开访问的，其它的URL都进行拦截控制  
+        if(url.contains("studentLogin.do")){  
+            return true;  
+        }  
+        //获取Session  
+        HttpSession session = request.getSession();  
+        Long studentId = (Long) session.getAttribute("studentId");  
+        System.out.println("studentId: "+studentId);
+        if(studentId != null){  
+            return true;  
+        }  
+        //不符合条件的，跳转到登录页  
+       // response.sendRedirect("index.jsp");
+        response.sendRedirect("/dormitory/login.jsp");   
+        System.out.println("forward to login.jsp");
+//		mv=new ModelAndView("redirect:/login/login.do",null); 
+        return false;  
+	}
+
+}
